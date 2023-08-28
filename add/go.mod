f8f1e5ec286fcb43c4e40f90a9fbbe20a8727f53
Mike Whalen <mike.whalen@lightstep.com>
2023-08-28 14:05:48 -0500

    init commit

---
+++ add/go.mod
@@ -0,0 +1,15 @@
+module github.com/mikewhalen/kubernetes-go-grpc
+
+go 1.19
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	google.golang.org/grpc v1.57.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ add/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+
+	"github.com/mikewhalen/kubernetes-go-grpc/pd/add"
+)
+
+func (s *server) Compute(cxt context.Context, r *add.AddRequest) (*add.AddResponse, error) {
+	result := &add.AddResponse{}
+	result.Result = r.A + r.B
+
+	logMessage := fmt.Sprintf("A: %d   B: %d     sum: %d", r.A, r.B, result.Result)
+	log.Println(logMessage)
+
+	return result, nil
+}
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen:  %v", err)
+	}
+
+	s := grpc.NewServer()
+	add.RegisterAddServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
